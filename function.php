7dc87095849f9e9fb352b6e4d225b44086301172
aubry <aubry.capitone@gmail.com>
2023-01-13 16:11:38 +0100

    afficher les articles ne marche pas encore array[] = $a => array_push

--- function.php
+++ function.php
@@ -24,3 +24,17 @@ function saveArticle(array $aArticle)
 {
     file_put_contents(SAVE_DIR.DIRECTORY_SEPARATOR.uniqid().'.json', json_encode($aArticle));
 }
+
+/**
+ * @return void
+ */
+function loadArticles():void
+{
+$aFilename=glob(SAVE_DIR.DIRECTORY_SEPARATOR.'*.json');
+foreach ($aFilename as $files) {
+    json_decode(file_get_contents($files), true);
+    echo
+        $files[subject] . "</br></br>" .
+        $files[content] . "</br></br>" .
+        $files[type] . "</br></br>";
+}
--- views/home.php
+++ views/home.php
@@ -2,14 +2,8 @@
 <h1>Mon Blog</h1>
     <?php
 
-    $aFilename=glob(SAVE_DIR.DIRECTORY_SEPARATOR.'*.json');
-        foreach ($aFilename as $files){
-            $sArticle=json_decode($files);
-            echo
-                $sArticle["subject"]."</br></br>".
-                $sArticle["content"]."</br></br>".
-                $sArticle["type"]."</br></br>";
-    }
+    print_r(loadArticles());
+
     ?>
 
 </main>
